2cd67dd556cba7ac2a607391c95c997fa02b9063
Xia Chen <18339953861@163.com>
2019-04-01 16:55:19 +0800

    装饰器表单校验

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import  {BrowserRouter as Router} from 'react-router-dom';
 
 import App from './App';
 
+import  './assets/less/reset.less';
 
 ReactDOM.render(<Router>
         <App />
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -6,12 +6,14 @@ import './index.less';
 import logo from './logo.png';
 
 
-const FormItem = Form.Item;
-export default  class  Login  extends Component{
+const Item = Form.Item;
+@Form.create()
+ class  Login  extends Component{
     handleSubmit = (e) => {
         e.preventDefault();
     }
     render() {
+        const { getFieldDecorator } = this.props.form;
         return (
            <div className="login">
                <header className="login-header">
@@ -21,20 +23,30 @@ export default  class  Login  extends Component{
                <section className="login-content">
                     <h3>用户登录</h3>
                    <Form onSubmit={this.handleSubmit} className="login-form">
-                       <FormItem>
-                           <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="用户名" />
-                       </FormItem>
-                       <FormItem>
+                       <Item>
+                           {getFieldDecorator('userName', {
+                               rules: [
+                                   { required: true, whitespace: true, message: '必须输入用户名!' },
+                                   {min: 4, message: '用户名必须大于4位'},
+                                   {max: 12, message: '用户名必须小于12位'},
+                                   {pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数组或下划线组成'}
+                               ]
+                           })(
+                                <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="用户名" />
+                           )}
+                       </Item>
+                       <Item>
                            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="密码" />
-                       </FormItem>
-                       <FormItem>
+                       </Item>
+                       <Item>
                            <Button type="primary" htmlType="submit" className="login-form-button">
                                登录
                            </Button>
-                       </FormItem>
+                       </Item>
                    </Form>
                </section>
            </div>
         );
     }
 }
+export default Login
