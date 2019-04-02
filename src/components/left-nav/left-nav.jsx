61e9fb0efa201a203929d11288a5f1179dddb1b0
Xia Chen <18339953861@163.com>
2019-04-02 23:55:17 +0800

    刷新页面菜单不跳转  展开显示二级菜单

--- src/components/left-nav/left-nav.jsx
+++ src/components/left-nav/left-nav.jsx
@@ -9,12 +9,19 @@ import  {withRouter,Link} from 'react-router-dom';
 const SubMenu = Menu.SubMenu;
 const Item = Menu.Item;
 
+// 装饰器 --> 向外暴露  withRouter(LeftNav) 生成新组建
+// withRouter作用：给非路由组件传递路由组件三个属性（history、location、match）
 @withRouter
  class  LeftNav  extends Component{
     render (){
+        //获取当前路径
+      const {pathname} = this.props.location;
       return (
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Item key="1">
+       // 1.为了刷新页面,当前选中菜单不会跳转到默认设置 而是和路由保持一致
+       // 需要将defaultSelectedKeys的值改成[pathname] ,同样key值也要修改为对应的路径
+          //2.为了展开显示二级菜单sub1  添加defaultOpenKeys={['sub1']}
+      <Menu theme="dark" defaultSelectedKeys={[pathname]} mode="inline" defaultOpenKeys={['sub1']}>
+          <Item key="/home">
               <Link to="/home">
               <Icon type="home" />
               <span>首页</span>
@@ -24,13 +31,13 @@ const Item = Menu.Item;
               key="sub1"
               title={<span><Icon type="appstore" /><span>商品</span></span>}
           >
-              <Item key="3">
+              <Item key="/category">
                   <Link to="/category">
                     <Icon type="bars" />
                     <span> 品类管理</span>
                   </Link>
               </Item>
-              <Item key="4">
+              <Item key="/product">
                   <Link to="/product">
                       <Icon type="tool" />
                       <span> 商品管理</span>
